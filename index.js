a8e59a75a40f34b49217cf4332a65d9f350ee45c
Jonah Attah <attahjonah93@gmail.com>
2024-10-18 12:43:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname, "index.html");
+
+const HOSTNAME ="localhost";
+const PORT = 1000;
+
+function requestHandler(req, res){
+    if (req.url === "/index.html" && req.method ==="GET"){
+        fs.readFile(filePath, "utf8", (err, data) =>{
+            if (err){
+                res.writeHead(400);
+                res.end("An error occured while reading the file");
+            } else {
+                res.writeHead(200);
+                res.end(data);
+                
+            }
+        });
+
+    } else {
+        if (req.url !== "/index.html" && req.method ==="*"){
+            res.writeHead(404);
+            res.end("File not found");
+        }
+    }
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(PORT, HOSTNAME, () =>{
+    console.log(`Server running at http://${HOSTNAME}:${PORT}`);
+});
