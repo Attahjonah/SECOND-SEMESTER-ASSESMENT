4374168a635dacfdc64c49a7dcf87ba4fa00d87f
Jonah Attah <attahjonah93@gmail.com>
2024-11-24 12:17:37 +0100

    final commit

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const { uptime } = require("process");
 
 const dbFilePath = path.join(__dirname, "db", "items.json");
 
-const DBFile = [];
+//const DBFile = [];
 
 const HOSTNAME = "localhost";
 const PORT = 8080;
@@ -47,14 +48,8 @@ function addItem(req, res){
 
     req.on("end", () =>{
         const parsedItem = Buffer.concat(body).toString();
-        const newItem = JSON.parse(parsedItem);
+        const newItem = JSON.parse(parsedItem);   
 
-        const lastItem = DBFile[DBFile.length -1];
-        const lastItemId = lastItem.id;
-        newItemId = lastItemId + 1;
-
-        
-        
         fs.readFile(dbFilePath, "utf8", (err, data) =>{
             if (err){
                 res.writeHead(404);
@@ -62,6 +57,7 @@ function addItem(req, res){
             }
 
             const existingItems = JSON.parse(data);
+            newItem.id = existingItems.length + 1;
             allItems = [...existingItems, newItem];
 
             fs.writeFile(dbFilePath, JSON.stringify(allItems), (err) =>{
@@ -74,6 +70,95 @@ function addItem(req, res){
         })
 
     });
+}
+
+// UPDATE ITEM
+
+function updateItem(req, res){
+    const body = [];
+    req.on("data", (chunk) =>{
+        body.push(chunk);
+    });
+    req.on("end", () =>{
+        const parsedDetail = Buffer.concat(body).toString(); 
+        const itemToUpdate = JSON.parse(parsedDetail);
+        const itemId = itemToUpdate.id;
+        
+        fs.readFile(dbFilePath, "utf8", (err, data) =>{
+            if (err){
+                res.writeHead(404);
+                res.end("Item not found");
+            }
+
+            const allItems = JSON.parse(data);
+            const itemIndex = allItems.findIndex(item => item.id === itemId)
+            if (itemIndex == -1){
+                res.writeHead(404);
+                res.end("Item not found");
+            }
+
+            const updatedItem = {...allItems[itemIndex], ...itemToUpdate};
+            allItems[itemIndex] = updatedItem
+            
+
+            
+             fs.writeFile(dbFilePath, JSON.stringify(allItems), (err) =>{
+                 if (err){
+                     res.writeHead(500);
+                     res.end(JSON.stringify({
+                        "message": "Server error"
+                     }))
+                 }
+                 res.writeHead(200);
+                 res.end("Update succesful");
+             });
+
+        });
+    })
+} 
+
+// DELETE ITEM
+function deleteItem(req, res){
+    const body = [];
+    req.on("data", (chunk) =>{
+        body.push(chunk);
+    });
+    req.on("end", () =>{
+        const parsedDetail = Buffer.concat(body).toString(); 
+        const itemToDelete = JSON.parse(parsedDetail);
+        const itemId = itemToDelete.id;
+        
+        fs.readFile(dbFilePath, "utf8", (err, data) =>{
+            if (err){
+                res.writeHead(404);
+                res.end("Item not found");
+            }
+
+            const allItems = JSON.parse(data);
+            const itemIndex = allItems.findIndex(item => item.id === itemId)
+            if (itemIndex == -1){
+                res.writeHead(404);
+                res.end("Item not found");
+            }
+
+            const deletedItem = {...allItems[itemIndex], ...itemToDelete};
+            allItems[itemIndex] = deletedItem
+            
+            allItems.splice(itemIndex, 1)
+             
+             fs.writeFile(dbFilePath, JSON.stringify(allItems), (err) =>{
+                 if (err){
+                     res.writeHead(500);
+                     res.end(JSON.stringify({
+                        "message": "Server error"
+                     }))
+                 }
+                 res.writeHead(200);
+                 res.end("Item deleted succesfully");
+             });
+
+        });
+    })
 }
 
 const server = http.createServer(requestListener);
